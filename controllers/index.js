cb9a2e06d9c184e2cff4b9c01a776bf9efefb1ad
jsong73 <jessicajinsong@gmail.com>
2022-10-23 23:18:59 -0400

    fixing my api routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,12 @@
 const router = require("express").Router();
 //importing the taskRoutes and userRoutes folders
 const taskRoutes = require("./taskRoutes");
-const userRoutes = require("./userRoutes")
+const userRoutes = require("./userRoutes");
 
 
 //localhost:5500/api/task
-// router.use("/task", taskRoutes);
+router.use("/task", taskRoutes);
+
 //localhost:5500/api/user
 router.use("/user", userRoutes);
 
